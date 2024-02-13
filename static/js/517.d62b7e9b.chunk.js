6cba9d58dcb5679f5dbacad17eb7d1702ddcd465
tanyadeineka <108138671+tanyadeineka@users.noreply.github.com>
2024-02-13 19:56:28 +0000

    Deploying to gh-pages from @ tanyadeineka/React-app@1a71484db4c1c91a3d60564c2fbeeaba86d92c3f 🚀

---
+++ static/js/517.d62b7e9b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[517],{517:function(e,n,r){r.r(n);r(791);var t=r(184);n.default=function(){return(0,t.jsxs)("div",{children:[(0,t.jsx)("header",{children:(0,t.jsx)("h1",{children:"Contact Us"})}),(0,t.jsx)("main",{children:(0,t.jsxs)("section",{children:[(0,t.jsx)("h2",{children:"Contact Information"}),(0,t.jsx)("p",{children:"Contact details: address, phone number, and email."})]})}),(0,t.jsx)("hr",{})]})}}}]);
+//# sourceMappingURL=517.d62b7e9b.chunk.js.map
