194826619d90e867f5086a919bc49c5cdc780476
Tetiana Deineka <108138671+tanyadeineka@users.noreply.github.com>
2024-02-13 21:47:11 +0200

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const data = [
+    {
+      companyName: "Stiedemann, Harris and Hegmann",
+      companyDescription: "grid-enabled",
+      id: "1"
+    },
+    {
+      companyName: "Hessel, Lockman and Yundt",
+      companyDescription: "4th generation",
+      id: "2"
+    },
+    {
+      companyName: "Rau - Larkin",
+      companyDescription: "intangible",
+      id: "3"
+    },
+    {
+      companyName: "Rempel, Littel and Auer",
+      companyDescription: "neutral",
+      id: "4"
+    },
+    {
+      companyName: "Botsford - Skiles",
+      companyDescription: "encompassing",
+      id: "5"
+    },
+    {
+      companyName: "Jenkins Group",
+      companyDescription: "24/7",
+      id: "6"
+    },
+    {
+      companyName: "Fadel - Carter",
+      companyDescription: "empowering",
+      id: "7"
+    },
+    {
+      companyName: "Bins Group",
+      companyDescription: "bandwidth-monitored",
+      id: "8"
+    },
+    {
+      companyName: "Mayert - Mayer",
+      companyDescription: "disintermediate",
+      id: "9"
+    },
+    {
+      companyName: "Sauer Group",
+      companyDescription: "next generation",
+      id: "10"
+    },
+    {
+      companyName: "Leuschke, Smitham and Marvin",
+      companyDescription: "homogeneous",
+      id: "11"
+    },
+    {
+      companyName: "Schimmel Group",
+      companyDescription: "intermediate",
+      id: "12"
+    },
+    {
+      companyName: "Fahey, Hills and Brakus",
+      companyDescription: "context-sensitive",
+      id: "13"
+    },
+    {
+      companyName: "Becker, Schulist and Lehner",
+      companyDescription: "multi-state",
+      id: "14"
+    },
+    {
+      companyName: "Heathcote LLC",
+      companyDescription: "zero administration",
+      id: "15"
+    }
+  ];
