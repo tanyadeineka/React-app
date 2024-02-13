64c240c7b0a10f9055a6a3b6dcea53fbf9923956
Tetiana Deineka <108138671+tanyadeineka@users.noreply.github.com>
2024-02-13 21:47:20 +0200

    Create HomePage.jsx

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to the Rindull</h1>
+      </header>
+
+      <main>
+        <section>
+          <h2>Services</h2>
+          <ul>
+            <li>Service 1</li>
+            <li>Service 2</li>
+            <li>Service 3</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Team</h2>
+          <ul>
+            <li>Member 1</li>
+            <li>Member 2</li>
+            <li>Member 3</li>
+          </ul>
+        </section>
+      </main>
+      <hr />
+    </div>
+  );
+};
+
+export default HomePage;
