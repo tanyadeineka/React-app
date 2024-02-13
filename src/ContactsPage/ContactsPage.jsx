1efd8a2cfe3ca8e1fd876510f91b8f9285b5f682
Tetiana Deineka <108138671+tanyadeineka@users.noreply.github.com>
2024-02-13 21:47:08 +0200

    Create ContactsPage.jsx

---
+++ src/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ContactsPage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Contact Us</h1>
+      </header>
+
+      <main>
+        <section>
+          <h2>Contact Information</h2>
+          <p>Contact details: address, phone number, and email.</p>
+        </section>
+      </main>
+      <hr />
+    </div>
+  );
+};
+
+export default ContactsPage;
